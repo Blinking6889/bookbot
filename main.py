c24490f0fc2932671aac24cbcc5198aec7ac289d
Crredsox13 <crredsox13@gmail.com>
2024-06-04 15:11:26 -0500

    fixed letter count

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ def wc(fc):
     return wc
 
 def lc(fc):
-    lc = {}
-    new_string = " ".jo
+    lc = []
     for i in fc.split():
-        lc[i] += 1
-    print(lc)
+        #lc[i] += 1
+        lc.append(i)
+    new_string = "".join(lc)
+    count = 0
+    for i in new_string:
+        count += 1
+    return(count)
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         #print(wc(file_contents))
         print(lc(file_contents))
+        #print file_contents
 
 
 if __name__ == "__main__":
